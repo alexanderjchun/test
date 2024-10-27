ca423bc05639d00f052e0965b72b8409f604cfd3
Alexander Chun <admin@alexanderjchun.com>
2024-10-27 07:19:40 -0700

    add script and playbook

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Set the URL for the Ansible repository
+ANSIBLE_REPO_URL="https://github.com/alexanderjchun/test.git"
+
+# Change to the home directory
+cd "$HOME"
+
+# Install Homebrew using their official install script
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Use Homebrew to install Ansible
+brew install ansible
+
+# Run ansible-pull to clone the repo and execute the playbook
+ansible-pull -U "$ANSIBLE_REPO_URL" playbooks/main.yml --ask-become-pass
