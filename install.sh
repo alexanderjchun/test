f0a438e9631b32018692e6ff969dab46d28ba59b
Alexander Chun <admin@alexanderjchun.com>
2024-10-27 08:01:23 -0700

    fix

--- install.sh
+++ install.sh
@@ -25,6 +25,11 @@ echo "Installing Ansible..."
 brew install ansible
 echo "Ansible installation complete"
 
+# Source .zprofile
+echo "Sourcing .zprofile..."
+. .zprofile
+echo "Sourcing .zprofile complete"
+
 # Run ansible-pull to clone the repo and execute the playbook
 echo "Running ansible-pull..."
 ansible-pull -U "https://github.com/alexanderjchun/test" playbooks/main.yml --ask-become-pass
